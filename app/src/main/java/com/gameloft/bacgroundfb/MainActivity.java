cbb266323a3defacd2c7d621e0f8f2db94f1a587
Letsee <levansy15t3@gmail.com>
2017-09-05 20:34:32 +0800

    Initial commit

---
+++ app/src/main/java/com/gameloft/bacgroundfb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gameloft.bacgroundfb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fb_backgroud);
+    }
+}
